d48879124b98e2c3c11f51d4d65ecbc36c8e7551
saramsey21 <saramsey21@gmail.com>
2022-12-22 13:40:54 -0500

    Navigation update

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, 'views')))
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
 
