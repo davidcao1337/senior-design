2e4c3665902c30c987766f09b379c55aefe26b8c
David Cao <davidcao1337@gmail.com>
2022-12-22 21:52:32 -0500

    Code refactoring
    Updated app to use React with Express/Node

--- routes/api/users.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-
-var router = express.Router();
-
-router.get("/", function(req, res){
-    res.json("This is a json status code for the users api");
-});
-
-module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ "users": ["user1", "user2", "user3"] })
+});
+
+app.listen(5000, () => { console.log("Server started on port 5000") });
