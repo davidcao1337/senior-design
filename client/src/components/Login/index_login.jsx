02e10dded7c2d379dac7890d0e3047d3c090dd75
David Cao <davidcao1337@gmail.com>
2023-01-14 16:08:42 -0500

    Update login .jsx files
    Added styling to login/register pages

--- client/src/components/Login/index_login.jsx
+++ client/src/components/Login/index_login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
-import Logo from '../../assets/lyfeon-green.png'
+import lyfeonLogo from '../../assets/lyfeon-green.png'
+import googleLogo from '../../assets/google-logo.png'
 import './login.css';
 export const Login = (props) => {
     const [email, setEmail] = useState('');
@@ -12,25 +13,31 @@ export const Login = (props) => {
 
     return(
         <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <img src={Logo} alt="" width="230" height="69" />
-            <h2>Login to your Account</h2>
-            <button>Continue with Google</button>
-            <p>---------- or Sign in with Email ----------</p>
+            <img className="mt-24 mb-10" src={lyfeonLogo} alt="" width="230" height="69" />
+            <h2 className="mb-5 font-bold text-3xl text-[#525252]">Login to your Account</h2>
+            <button className="border-2 rounded-[5px] px-16 py-3">
+                <img className="float-left mr-5" src={googleLogo} alt="" width="20" height="20" />
+                <p className="float-right font-bold text-[#828282]">Continue with Google</p>
+            </button>
+            <p className="m-10 font-semibold text-[#A1A1A1]">---------- or Sign in with Email ----------</p>
             <div className="auth-form-container">
                 <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htlmfor="email">Email</label>
-                        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="myemailaddress@gmail.com" id="email" name="email" />
-                        <label htlmfor="password">Password</label>
-                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                        </div>
-                    <div>
-                        <p><input type="checkbox" />Remember Me</p>
-                        <button>Forgot Password?</button>
+                    <div className="text-left text-[#828282]">
+                        <label className="font-semibold" htlmfor="email">Email</label>
+                        <input className="mb-6 border-2 w-full rounded-[5px] px-2 py-2" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="myemailaddress@gmail.com" id="email" name="email" />
+                        <label className="font-semibold" htlmfor="password">Password</label>
+                        <input className="border-2 w-full rounded-[5px] px-2 py-2" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                     </div>
-                    <button type="submit">Log In</button>
+                    <div className="mt-2">
+                        <p className="float-left text-[#A1A1A1]"><input className="accent-lyfeon-green checked:checkmark-white" type="checkbox" />  Remember Me</p>
+                        <button className="float-right text-semibold text-lyfeon-green">Forgot Password?</button>
+                    </div>
+                    <button className="mt-10 mb-24 w-full font-extrabold text-lg text-white bg-lyfeon-green rounded-[6px] px-16 py-3" type="submit">Log In</button>
                 </form>
-                <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
+                <div className="mt-24">
+                    <p className="text-[#828282]">Not registered yet?</p>
+                    <button className="text-semibold text-lyfeon-green" onClick={() => props.onFormSwitch('register')}>Create an account</button>
+                </div>
             </div>
         </div>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lyfeon-green': '#18B283'
+      },
+    },
   },
   plugins: [],
 }
