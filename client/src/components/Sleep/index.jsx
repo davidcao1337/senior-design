62d87560314269e65e634f5562786ffceec78ab3
Youmin <772069508@qq.com>
2023-01-19 14:42:23 -0500

    Installed ChartJs & testing push

--- client/src/components/Sleep/index.jsx
+++ client/src/components/Sleep/index.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { Bar } from "react-chartjs-2";
 
 const Sleep = () => {
     useEffect( ()=> {
@@ -14,16 +15,50 @@ const Sleep = () => {
     };
 
     return (
-        <section>
-            {
-                items.map(item => (
-                    <div>
-                        <p>{item.name}</p>
-                        <p>{item.msg}</p>
-                    </div>
-                ))
-            }
-        </section>
+        <div className="App">
+          <h1>GEEKSFORGEEKS BAR CHART REACTJS</h1>
+          <div style={{ maxWidth: "650px" }}>
+            <Bar
+              data={{
+                // Name of the variables on x-axies for each bar
+                labels: ["1st bar", "2nd bar", "3rd bar", "4th bar"],
+                datasets: [
+                  {
+                    // Label for bars
+                    label: "total count/value",
+                    // Data or value of your each variable
+                    data: [1552, 1319, 613, 1400],
+                    // Color of each bar
+                    backgroundColor: ["aqua", "green", "red", "yellow"],
+                    // Border color of each bar
+                    borderColor: ["aqua", "green", "red", "yellow"],
+                    borderWidth: 0.5,
+                  },
+                ],
+              }}
+              // Height of graph
+              height={400}
+              options={{
+                maintainAspectRatio: false,
+                scales: {
+                  yAxes: [
+                    {
+                      ticks: {
+                        // The y-axis value will start from zero
+                        beginAtZero: true,
+                      },
+                    },
+                  ],
+                },
+                legend: {
+                  labels: {
+                    fontSize: 15,
+                  },
+                },
+              }}
+            />
+          </div>
+        </div>
     );
 }
 
