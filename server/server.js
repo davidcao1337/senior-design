3c3d5a60ce829584601455adf13f09ebe7e35d5d
David Cao <davidcao1337@gmail.com>
2023-01-20 15:14:22 -0500

    Update server.js
    Added connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import routesHandler from "./routes/handler.js"
 
 
 const app = express();
+
+const uri = 'mongodb+srv://lyfeonadmin:lfdPxA378lJdsQ9o@lyfeoncluster.7xvhbkd.mongodb.net/?retryWrites=true&w=majority'
+
+async function connect() {
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+connect();
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler)
