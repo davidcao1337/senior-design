2de424de029a2f1d5fa24fcddf6fe04049b8c8ac
Youmin <772069508@qq.com>
2023-01-19 20:13:32 -0500

    Rough sleep tracker & merge test

--- client/src/components/Sleep/index.jsx
+++ client/src/components/Sleep/index.jsx
@@ -1,3 +1,4 @@
+/*
 import React, {useEffect, useState} from 'react';
 
 const Sleep = () => {
@@ -15,6 +16,7 @@ const Sleep = () => {
 
     return (
         <section>
+            <content>
             {
                 items.map(item => (
                     <div>
@@ -23,8 +25,36 @@ const Sleep = () => {
                     </div>
                 ))
             }
+            </content>
         </section>
     );
+}
+
+export default Sleep;
+*/
+
+import React, { useState } from "react"
+import './sleep.css';
+export const Sleep = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return(
+        <div className="flex flex-col items-left justify-left w-full flex-1 px-20 text-left">
+            <h2 className="mb-5 font-bold text-3xl text-[#525252]">Sleep Tracking</h2>
+            <div class="btn-group">
+                <input type="radio" name="options" data-title="Day" class="btn" />
+                <input type="radio" name="options" data-title="Week" class="btn" />
+                <input type="radio" name="options" data-title="Month" class="btn" />
+                <input type="radio" name="options" data-title="Year" class="btn" />
+            </div>
+        </div>
+    )
 }
 
 export default Sleep;
