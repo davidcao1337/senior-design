b6a142f97d7e29819896235f7466d7ea97e5c0c5
David Cao <davidcao1337@gmail.com>
2023-01-25 17:20:20 -0500

    Create userModel.js

---
+++ server/models/userModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema)
