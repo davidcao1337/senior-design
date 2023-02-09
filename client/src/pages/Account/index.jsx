8473c975d83ac64d31dbfa5f2e07df2000d04f56
David Cao <davidcao1337@gmail.com>
2023-02-09 14:18:21 -0500

    Update Account/index.jsx
    Added menu

--- client/src/pages/Account/index.jsx
+++ client/src/pages/Account/index.jsx
@@ -9,6 +9,13 @@ const Account = () => {
             <content>
                 <div>
                     <h1 className="Title">My Account</h1>
+                    <div className="my-account-nav">
+                        <ul className="menu bg-base-100 w-56 text-[#748AA1] rounded-md active:after:">
+                            <li><a className="active: bg-lyfeon-green text-white">Profile</a></li>
+                            <li><a>Account Settings</a></li>
+                            <li><a>Privacy</a></li>
+                        </ul>
+                    </div>
                 </div>
             </content>
         </section>
