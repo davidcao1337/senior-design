ecf781144f71fdf52b980783452e78e196340dd2
Twisha <twisha.anshu@gmail.com>
2023-02-14 23:49:37 -0500

    fixed sleep chart on dashboard + style

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -19,15 +19,17 @@ const Dashboard = () => {
     return (
         <section className="dashboard">
             <NavBar />
+            <div className="dashboard-header">My Dashboard</div>
             <div className="content">
                 {items.map(item => (
-                    <div key={item.id}>
+                    <div key={item.id} >
                         <p>{item.name}</p>
                         <p>{item.msg}</p>
                     </div>
                 ))}
             </div>
             <div className="chart">
+                <h2 className="chart-label">Your Sleep Recently:</h2>
                 <SleepBarChart />
             </div>
         </section>
