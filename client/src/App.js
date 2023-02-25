25485f03df0b85e5e62ba75580617f6b3182f05d
Twisha <twisha.anshu@gmail.com>
2023-02-25 15:50:12 -0500

    updated git ignore anf package-lock and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext';
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext'
 import { Login } from './pages/Login/index_login';
 import { Register } from './pages/Login/index_register';
-import Profile from './pages/Account/index_profile';
-import Settings from './pages/Account/index_settings';
-import Privacy from './pages/Account/index_privacy';
-import Notifications from './pages/Account/index_notifications';
-import Preferences from './pages/Account/index_preferences'
 import Dashboard from './pages/Dashboard';
 import Exercise from './pages/Exercise';
 import Nutrition from './pages/Nutrition';
@@ -21,25 +16,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Navigate to="/login" />} /> {/* TODO: If user is authenticated, navigate to /dashboard */}
-            <Route path="/login" element={<Login />} /> 
-            <Route path="/register" element={<Register />} /> 
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/exercise" element={<Exercise />} /> 
-            <Route path="/nutrition" element={<Nutrition />} /> 
-            <Route path="/sleep" element={<Sleep />} />
-       {/*    <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
+          <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
           <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
           <Route path="/register" element={!user ? <Register /> : <Navigate to="/dashboard" />} />
-          <Route path="/account/profile" element={user ? <Profile /> : <Navigate to="/login" />} />
-          <Route path="/account/settings" element={user ? <Settings /> : <Navigate to="/login" />} />
-          <Route path="/account/privacy" element={user ? <Privacy /> : <Navigate to="/login" />} />
-          <Route path="/account/notifications" element={user ? <Notifications /> : <Navigate to="/login" />} />
-          <Route path="/account/preferences" element={user ? <Preferences /> : <Navigate to="/login" />} />
           <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
           <Route path="/exercise" element={user ? <Exercise /> : <Navigate to="/login" />} />
           <Route path="/nutrition" element={user ? <Nutrition /> : <Navigate to="/login" />} />
-          <Route path="/sleep" element={user ? <Sleep /> : <Navigate to="/login" />} /> */}
+          <Route path="/sleep" element={user ? <Sleep /> : <Navigate to="/login" />} />
         </Routes>
       </BrowserRouter>
     </div>
