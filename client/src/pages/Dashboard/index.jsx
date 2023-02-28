088bf03cabe83d739e570af12c1bea441741f5ed
TwishaAnshu <43499131+TwishaAnshu@users.noreply.github.com>
2023-02-28 16:24:09 -0500

    dashbard and right panel updated

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -1,35 +1,64 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../../components/NavBar';
+import SleepBarChart from '../../components/Charts/SleepBarChart';
 import './dashboard.css';
+import WeightLineChart from '../../components/Charts/WeightLineChart';
+import RightPanel from '../../components/RightPanel';
 
 const Dashboard = () => {
-    useEffect( ()=> {
+    useEffect(() => {
         fetchItems();
     }, []);
 
     const [items, setItems] = useState([]);
 
-    const fetchItems = async() => {
+    const fetchItems = async () => {
         const data = await fetch('/dashboard');
         const items = await data.json();
         setItems(items);
     };
 
     return (
-        <section>
+        <section className="dashboard">
             <NavBar />
-            <content>
-            {
-                items.map(item => (
-                    <div>
+            <div className="dashboard-header">My Dashboard</div>
+            {/* <div className="content">
+                {items.map(item => (
+                    <div key={item.id}>
                         <p>{item.name}</p>
                         <p>{item.msg}</p>
                     </div>
-                ))
-            }
-            </content>
+                ))}
+            </div> */}
+            <div className="charts">
+                <h2 className="chart-label">Your Sleep Recently:</h2>
+                <SleepBarChart height={100} width={600} />
+            </div>
+
+            <div className="chart">
+                <h2 className="chart-label">Your Weight Over Time:</h2>
+                <WeightLineChart height={100} width={600} />
+            </div>
+
+            <div className="nutrition-breakdown">
+                <h2 className="chart-label">Nutrition Breakdown</h2>
+                <div className="nutrition-row">
+                    <div className="nutrition-label">Protein:</div>
+                    <div className="nutrition-value">24g</div>
+                </div>
+                <div className="nutrition-row">
+                    <div className="nutrition-label">Carbs:</div>
+                    <div className="nutrition-value">50g</div>
+                </div>
+                <div className="nutrition-row">
+                    <div className="nutrition-label">Fat:</div>
+                    <div className="nutrition-value">12g</div>
+                </div>
+            </div>
+
+            <RightPanel />
         </section>
     );
-}
+};
 
 export default Dashboard;
