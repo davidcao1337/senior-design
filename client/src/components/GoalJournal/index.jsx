c8daad9b8dfc98ef0c4c3b725dda053ee541dfc4
David Cao <davidcao1337@gmail.com>
2023-03-06 16:30:49 -0500

    Update index.jsx
    Added map function for goals

--- client/src/components/GoalJournal/index.jsx
+++ client/src/components/GoalJournal/index.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisVertical, faPlus, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faDumbbell, faEllipsisVertical, faMoon, faPlus, faTrophy, faUtensils } from '@fortawesome/free-solid-svg-icons';
 
 const GoalJournal = () => {
+
+    const [goals, setGoals] = useState(null)
+    const [goalIcon, setGoalIcon] = useState(faTrophy)
+
+    useEffect(() => {
+        const fetchGoals = async () => {
+            const response = await fetch('/goals')
+            const json = await response.json()
+
+            if (response.ok) {
+                setGoals(json)
+            }
+        }
+
+        fetchGoals()
+    }, [])
+
     return (
         <div className="goals-container card bg-base-100">
             <div className="card-body">
@@ -9,16 +27,25 @@ const GoalJournal = () => {
                     <h2>Goals</h2>
                 </div>
                 <div className="goal-cards flex flex-col space-y-8">
-                    <div className="card border border-gray-300">
-                        <div className="card-body">
-                            <div className="flex flex-row">
-                                <FontAwesomeIcon className="mr-5" icon={faTrophy} size="2x" />
-                                <p>Goal Text</p>
-                                <div className="ml-20 mr-20" />
-                                <button className="ml-20"><FontAwesomeIcon icon={faEllipsisVertical} /></button>
+                    {goals && goals.map((goal) => (
+                        <div className="card border border-gray-300">
+                            <div key={goal._id} className="card-body">
+                                <div className="flex flex-row">
+                                    <FontAwesomeIcon className="mr-5" size="2x" 
+                                    icon={
+                                        goal.goalType == 'exercise' ? faDumbbell
+                                        : goal.goalType == 'nutrition' ? faUtensils
+                                        : goal.goalType == 'sleep' ? faMoon
+                                        : faTrophy
+                                    }
+                                    />
+                                    <p>{goal.description}</p>
+                                    <div className="ml-20 mr-20" />
+                                    <button className="ml-20"><FontAwesomeIcon icon={faEllipsisVertical} /></button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    ))}
                     <div className="card border border-gray-300 border-dashed">
                         <div className="card-body">
                             <button>
