0f6b661cd3be7072692fac5cc4110054b199d5c6
David Cao <davidcao1337@gmail.com>
2023-03-21 13:45:39 -0400

    Update login and register pages
    Changes:
    -Fixed layout issues
    -Removed Google Sign-In button

--- client/src/pages/Login/index_register.jsx
+++ client/src/pages/Login/index_register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import lyfeonLogo from '../../assets/lyfeon-green.png'
-import googleLogo from '../../assets/google-logo.png'
 import './login.css'
 import { useNavigate } from "react-router-dom"
 import { useRegister } from "../../hooks/useRegister"
@@ -26,21 +25,20 @@ export const Register = () => {
     return(
         <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
             <img className="mt-24 mb-10" src={lyfeonLogo} alt="" width="230" height="69" />
-            <h2 className="mb-5 font-bold text-3xl text-[#525252]">Login to your Account</h2>
-            <button className="border-2 rounded-[5px] px-16 py-3">
-                <img className="float-left mr-5" src={googleLogo} alt="" width="20" height="20" />
-                <p className="float-right font-bold text-[#828282]">Continue with Google</p>
-            </button>
-            <p className="m-10 font-semibold text-[#A1A1A1]">---------- or Sign up with Email ----------</p>
-            <div className="auth-form-container">
+            <h2 className="mb-5 font-bold text-3xl text-[#525252]">Sign Up for an Account</h2>
+            <div className="auth-form-container w-1/4">
                 <form onSubmit={handleSubmit}>
-                    <div className="text-left text-[#828282]">
+                    <div className="name-container text-left text-[#828282]">
                         <label className="font-semibold" htlmfor="name">Full Name</label>
-                        <input className="mb-6 border-2 w-full rounded-[5px] px-2 py-2" value={name} onChange={(e) => setName(e.target.value)} placeholder="Full Name" id="name" name="name" />
+                        <input className="mb-6 border-2 w-full rounded-[5px] px-2 py-2" value={name} onChange={(e) => setName(e.target.value)} id="name" name="name" />
+                    </div>
+                    <div className="email-container text-left text-[#828282]">
                         <label className="font-semibold" htlmfor="email">Email</label>
-                        <input className="mb-6 border-2 w-full rounded-[5px] px-2 py-2" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="myemailaddress@gmail.com" id="email" name="email" />
+                        <input className="mb-6 border-2 w-full rounded-[5px] px-2 py-2" value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="email" name="email" />
+                    </div>
+                    <div className="password-container text-left text-[#828282]">
                         <label className="font-semibold" htlmfor="password">Password</label>
-                        <input className="border-2 w-full rounded-[5px] px-2 py-2" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                        <input className="border-2 w-full rounded-[5px] px-2 py-2" value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="password" />
                     </div>
                     <button className="mt-10 mb-24 w-full font-extrabold text-lg text-white bg-lyfeon-green rounded-[6px] px-16 py-3" type="submit" disabled={isLoading}>Register</button>
                     {error && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">{error}</div>}
