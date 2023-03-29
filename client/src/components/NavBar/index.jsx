6e02aceb0468d92c4897b5644d1eb737449639ff
David Cao <davidcao1337@gmail.com>
2023-03-29 18:16:30 -0400

    Update index.jsx

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -7,6 +7,9 @@ import { faDumbbell, faUtensils, faMoon, faChartSimple, faArrowRightFromBracket 
 import DefaultAvatar from "../../assets/circle-user-solid.svg"
 
 const NavBar = () => {
+    const activeLink = "active: bg-lyfeon-green text-white"
+    const normalLink = ""
+
     var user_id = ""
     if (localStorage.getItem('user') != null) {
         user_id = JSON.parse(localStorage.getItem('user')).user_id
@@ -39,14 +42,14 @@ const NavBar = () => {
                     <div className="mt-5 flex items-center justify-center">
                       <Link to='/account/profile'><img src={userAvatar} height="40" width="40" /></Link>
                     </div>
-                    <ul className="navItems">
-                      <Link to='/dashboard'><li><span><FontAwesomeIcon icon={faChartSimple} size="2x" className="navIcon"></FontAwesomeIcon></span></li></Link>
-                      <Link to='/exercise'><li><span><FontAwesomeIcon icon={faDumbbell} size="2x" className="navIcon"></FontAwesomeIcon></span></li></Link>
-                      <Link to='/nutrition'><li><span><FontAwesomeIcon icon={faUtensils} size="2x" className="navIcon"></FontAwesomeIcon></span></li></Link>
-                      <Link to='/sleep'><li><span><FontAwesomeIcon icon={faMoon} size="2x" className="navIcon"></FontAwesomeIcon></span></li></Link>
+                    <ul className="menu mt-28 bg-base-100 w-full text-[#748AA1] rounded-md float-left">
+                      <NavLink to='/dashboard' className={({ isActive }) => (isActive ? activeLink + " rounded-tr-md rounded-br-md" : normalLink)}><li><span><FontAwesomeIcon icon={faChartSimple} size="2x" className="navIcon ml-2"></FontAwesomeIcon></span></li></NavLink>
+                      <NavLink to='/exercise' className={({ isActive }) => (isActive ? activeLink + " rounded-tr-md rounded-br-md" : normalLink)}><li><span><FontAwesomeIcon icon={faDumbbell} size="2x" className="navIcon ml-1"></FontAwesomeIcon></span></li></NavLink>
+                      <NavLink to='/nutrition' className={({ isActive }) => (isActive ? activeLink + " rounded-tr-md rounded-br-md" : normalLink)}><li><span><FontAwesomeIcon icon={faUtensils} size="2x" className="navIcon ml-2"></FontAwesomeIcon></span></li></NavLink>
+                      <NavLink to='/sleep' className={({ isActive }) => (isActive ? activeLink + " rounded-tr-md rounded-br-md" : normalLink)}><li><span><FontAwesomeIcon icon={faMoon} size="2x" className="navIcon ml-2"></FontAwesomeIcon></span></li></NavLink>
                     </ul>
                     <div className="logout ml-2">
-                      <button onClick={handleClick}><FontAwesomeIcon icon={faArrowRightFromBracket} size="2x" color="#B8C5D3" /></button> {/* TEMP; Change location later */}
+                      <button onClick={handleClick}><FontAwesomeIcon icon={faArrowRightFromBracket} size="2x" color="#748AA1" /></button> {/* TEMP; Change location later */}
                     </div>
                   </div>
                   
