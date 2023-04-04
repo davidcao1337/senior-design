8a0a8ec85536e6da4028fb12f5e5c6b0141ae2af
TwishaAnshu <43499131+TwishaAnshu@users.noreply.github.com>
2023-04-04 17:35:34 -0400

    no nutrition data

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -4,27 +4,36 @@ import SleepBarChart from '../../components/Charts/SleepBarChart';
 import './dashboard.css';
 import WeightLineChart from '../../components/Charts/WeightLineChart';
 import RightPanel from '../../components/RightPanel';
-//import RightPanel from '../../components/RightPanel/sleepRightPanel'
 import NutritionProgressBar from './NutritionProgressBar';
-import ProgressBar from '../Nutrition/ProgressBar';
-import index from '../Nutrition/index';
 import { useFoodItemContext } from '../../hooks/useFoodItemContext';
-import { useAuthContext } from '../../hooks/useAuthContext';
-
-
 
 const Dashboard = () => {
-    useEffect(() => {
-        fetchItems();
-    }, []);
+    const { foodItems } = useFoodItemContext();
 
-    const [items, setItems] = useState([]);
+    // Calculate total macro and calorie intake
+    const [caloriesEaten, setCaloriesEaten] = useState(0);
+    const [proteinEaten, setProteinEaten] = useState(0);
+    const [carbsEaten, setCarbsEaten] = useState(0);
+    const [fatEaten, setFatEaten] = useState(0);
 
-    const fetchItems = async () => {
-        const data = await fetch('/dashboard');
-        const items = await data.json();
-        setItems(items);
-    };
+/*     useEffect(() => {
+        // Reset values
+        setCaloriesEaten(0);
+        setProteinEaten(0);
+        setCarbsEaten(0);
+        setFatEaten(0);
+
+        // Calculate total values from food items
+        foodItems.forEach((item) => {
+            setCaloriesEaten((caloriesEaten) => caloriesEaten + item.calories);
+            setProteinEaten((proteinEaten) => proteinEaten + item.protein);
+            setCarbsEaten((carbsEaten) => carbsEaten + item.carbs);
+            setFatEaten((fatEaten) => fatEaten + item.fat);
+        });
+    }, [foodItems]); */
+
+    // Calculate total macro calories
+    const macroCals = proteinEaten * 4 + carbsEaten * 4 + fatEaten * 9;
 
     return (
         <section className="dashboard">
@@ -34,59 +43,40 @@ const Dashboard = () => {
                     <div> My Dashboard</div>
                 </titleContainer>
 
-                {/* <div className="dashboard-header">My Dashboard</div> */}
-                {/* <div className="content">
-                {items.map(item => (
-                    <div key={item.id}>
-                        <p>{item.name}</p>
-                        <p>{item.msg}</p>
-                    </div>
-                ))}
-            </div> */}
-
-                {/* <div className="Pbar">
-                    <h2 className="chart-label">Nutrition breakdown2:</h2>
-                    <ProgressBar props={{ caloriesEaten, macroCals, color }} />
-                    
-                </div> */}
-
                 <div className="Pbar">
-                    <h2 className="chart-label">Nutrition breakdown2:</h2>
-                    <NutritionProgressBar />
+                    <h2 className="chart-label">Nutrition Breakdown:</h2>
+                    <NutritionProgressBar
+                        caloriesEaten={caloriesEaten}
+                        macroCals={macroCals}
+                    />
                 </div>
-
-
-
 
                 <div className="charts">
                     <h2 className="chart-label">Your Sleep Recently:</h2>
                     <SleepBarChart />
-                    {/* <SleepBarChart height={100} width={600} /> */}
                 </div>
 
                 <div className="chart">
                     <h2 className="chart-label">Your Weight Over Time:</h2>
-                    <WeightLineChart height={100} width={600} />
+                    <WeightLineChart />
                 </div>
 
-                <div className="nutrition-breakdown">
-                    <h2 className="chart-label">Nutrition Breakdown</h2>
-                    <div className="nutrition-row">
-                        <div className="nutrition-label">Protein:</div>
-                        <div className="nutrition-value">24g</div>
-                    </div>
-                    <div className="nutrition-row">
-                        <div className="nutrition-label">Carbs:</div>
-                        <div className="nutrition-value">50g</div>
-                    </div>
-                    <div className="nutrition-row">
-                        <div className="nutrition-label">Fat:</div>
-                        <div className="nutrition-value">12g</div>
-                    </div>
-                </div>
-
+                {/* <div className="nutrition-breakdown">
+          <h2 className="chart-label">Nutrition Breakdown</h2>
+          <div className="nutrition-row">
+            <div className="nutrition-label">Protein:</div>
+            <div className="nutrition-value">{proteinEaten}g</div>
+          </div>
+          <div className="nutrition-row">
+            <div className="nutrition-label">Carbs:</div>
+            <div className="nutrition-value">{carbsEaten}g</div>
+          </div>
+          <div className="nutrition-row">
+            <div className="nutrition-label">Fat:</div>
+            <div className="nutrition-value">{fatEaten}g</div>
+          </div>
+        </div> */}
             </content>
-
 
             <RightPanel />
         </section>
