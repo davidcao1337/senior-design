4a7ba71755293e523f86ef815644ee94f5991306
TwishaAnshu <43499131+TwishaAnshu@users.noreply.github.com>
2023-04-04 20:29:14 -0400

    ProteinBar data fix

--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -4,17 +4,81 @@ import SleepBarChart from '../../components/Charts/SleepLineChart';
 import './dashboard.css';
 import WeightLineChart from '../../components/Charts/WeightLineChart';
 import RightPanel from '../../components/RightPanel';
-import NutritionProgressBar from './NutritionProgressBar';
 import { useFoodItemContext } from '../../hooks/useFoodItemContext';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import ProgressBar from '../Nutrition/ProgressBar';
 
 const Dashboard = () => {
-    const { foodItems } = useFoodItemContext();
 
-    // Calculate total macro and calorie intake
-    const [caloriesEaten, setCaloriesEaten] = useState(0);
-    const [proteinEaten, setProteinEaten] = useState(0);
-    const [carbsEaten, setCarbsEaten] = useState(0);
-    const [fatEaten, setFatEaten] = useState(0);
+    const { foodItems, dispatch } = useFoodItemContext();
+
+    const { user } = useAuthContext();
+
+    useEffect(() => {
+        const fetchFoodItems = async () => {
+            const response = await fetch('/nutrition', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
+            const json = await response.json()
+
+            if (response.ok) {
+                dispatch({ type: 'SET_FOOD', payload: json })
+            }
+        }
+
+        
+
+        if (user) {
+            fetchFoodItems();
+        }
+    }, [dispatch, user]);
+
+    let totalCals = 0
+
+    let totalProtein = 0
+
+    let totalFat = 0
+
+    let totalCarbs = 0
+
+    let proteinCals = 0
+
+    let fatCals = 0
+
+    let carbCals = 0
+
+
+
+    if (foodItems) {
+
+        for (let i = 0; i < foodItems.length; i++) {
+
+            // seperate into individual arrays
+
+            totalCals = totalCals + (foodItems[i].servingsEaten * foodItems[i].caloriesPerServing)
+
+            totalProtein = totalProtein + (foodItems[i].servingsEaten * foodItems[i].proteinPerServing)
+
+            totalFat = totalFat + (foodItems[i].servingsEaten * foodItems[i].fatPerServing)
+
+            totalCarbs = totalCarbs + (foodItems[i].servingsEaten * foodItems[i].carbsPerServing)
+
+        }
+        
+        proteinCals = totalProtein * 4
+
+        fatCals = totalFat * 9
+
+        carbCals = totalCarbs * 4
+
+
+
+
+    }
+
+ 
 
 /*     useEffect(() => {
         // Reset values
@@ -32,8 +96,7 @@ const Dashboard = () => {
         });
     }, [foodItems]); */
 
-    // Calculate total macro calories
-    const macroCals = proteinEaten * 4 + carbsEaten * 4 + fatEaten * 9;
+
 
     return (
         <section className="dashboard">
@@ -43,12 +106,25 @@ const Dashboard = () => {
                     <div> My Dashboard</div>
                 </titleContainer>
 
+                <div className='macroGraphs'>
+
+                    <protein> Protein </protein>
+
+                    <ProgressBar props={{ caloriesEaten: totalCals, macroCals: proteinCals, color: "#6a1b9a" }} />
+
+                    <carbs> Carbs </carbs>
+
+                    <ProgressBar props={{ caloriesEaten: totalCals, macroCals: carbCals, color: "#00695c" }} />
+
+                    <fat> Fat </fat>
+
+                    <ProgressBar props={{ caloriesEaten: totalCals, macroCals: fatCals, color: "#ef6c00" }} />
+
+                </div>
+
                 <div className="Pbar">
                     <h2 className="chart-label">Nutrition Breakdown:</h2>
-                    <NutritionProgressBar
-                        caloriesEaten={caloriesEaten}
-                        macroCals={macroCals}
-                    />
+
                 </div>
 
                 <div className="charts">
